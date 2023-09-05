7564d9ffeb50fc15ef0be1958e7a8e3c168d740a
Tulasi Kanish <90704790+kanishtulasi@users.noreply.github.com>
2023-09-05 18:11:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").textContent = "🥳 Player 1 Wins!";
+}
+else if(randomNumber1<randomNumber2)
+{
+  document.querySelector("h1").textContent = "Player 2 Wins! 🥳";
+}
+else
+{
+  document.querySelector("h1").textContent = "Draw! 🙂";
+}
